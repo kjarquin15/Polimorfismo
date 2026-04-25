d58922f147dfece2f15e304e8019ead728dc516a
KAREN LIZZETH JARQUÍN SALGUERO <kjarquins@miumg.edu.gt>
2026-04-24 18:40:05 -0600

    Clases figura y rectangulo

---
+++ src/polimorfismo/Rectangulo.java
@@ -0,0 +1,16 @@
+package polimorfismo;
+
+public class Rectangulo extends Figura {
+    private double base, altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+        return base * altura;
+    }
+}
+
